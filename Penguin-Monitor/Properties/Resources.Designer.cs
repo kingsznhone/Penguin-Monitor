7964650f1645081602565fb3fe74aa7f57cb3eff
kingsznhone <35349300+kingsznhone@users.noreply.github.com>
2020-04-20 00:27:47 +0800

    hotfix

--- Penguin-Monitor/Properties/Resources.Designer.cs
+++ Penguin-Monitor/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace Penguin_Monitor.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Bitmap mp {
-            get {
-                object obj = ResourceManager.GetObject("mp", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
